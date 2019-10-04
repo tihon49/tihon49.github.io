52900e5a809d10ddaf78cfd6c181de55adc42fe5
Tihon49 <tihon49@gmail.com>
2019-10-04 14:17:13 +0300

    добавил шутку про бухого

--- test.js
+++ test.js
@@ -1,25 +1,27 @@
-let name1  = document.querySelectorAll('input')[0],
-    liters1 = document.querySelectorAll('input')[1],
-    name2  = document.querySelectorAll('input')[2],
-    liters2 = document.querySelectorAll('input')[3],
-    out    = document.querySelector('.out'),
-    button = document.querySelector('button'),
-    clearBtn = document.querySelector('.clear'),
-    overlay = document.querySelector('.overlay'),
-    overlayBtn = document.querySelector('.overlayBtn'),
-    qwestionBtn = document.querySelector('.qwestion'),
+let price1  = document.querySelectorAll('input')[0],    //цена 1
+    liters1 = document.querySelectorAll('input')[1],    //литраж 1 
+    price2  = document.querySelectorAll('input')[2],    //цена 2
+    liters2 = document.querySelectorAll('input')[3],    //литраж 2
+    out    = document.querySelector('.out'),            //вывод итога
+    button = document.querySelector('button'),          //кнопка "расчитать"
+    clearBtn = document.querySelector('.clear'),        //кнопка "сбросить"
+    overlay = document.querySelector('.overlay'),       //оверлэй
+    overlayBtn = document.querySelector('.overlayBtn'), //кнопка "я все понял"
+    qwestionBtn = document.querySelector('.qwestion'),  //кнопка "че за...?"
+    count = 0,                                          //счетчик
 
-    sum1 = 0,
-    sum2 = 0,
-    litersSum1 = 0,
+    sum1 = 0,                                           //значение из цены 1
+    sum2 = 0,                                           //значение из цены 2
+    litersSum1 = 0,                             
     litersSum2 = 0,
     totalPrice = 0;
 
-name1.addEventListener('change', function() {
+//получаем значения из полей
+price1.addEventListener('change', function() {
     sum1 = +this.value;
 });
 
-name2.addEventListener('change', function() {
+price2.addEventListener('change', function() {
     sum2 = +this.value;
 });
 
@@ -31,40 +33,57 @@ liters2.addEventListener('change', function() {
     litersSum2 = +this.value;
 })
 
+
+//функция расчета
 function getTotalPrice() {
     totalPrice = sum1 * litersSum2 / litersSum1;
     console.log(totalPrice);
     return totalPrice;
 }
 
+
+//кнопка "расчитать" с проверкой введенных данных
 button.addEventListener('click', function() {
     getTotalPrice();
     if (isNaN(totalPrice)) {
         out.style.fontSize = '20px';
         out.textContent = 'не верные данные...';
+        count++;
     } else {
         out.textContent = totalPrice.toFixed() + ' руб.';
-        name1.style.boxShadow = '0 0 20px red';
+        price1.style.boxShadow = '0 0 20px red';
         out.style.boxShadow = '0 0 20px red';
         liters2.style.boxShadow = '0 0 20px red';
     }
+
+    if (count > 3) {
+        out.style.fontSize = '20px';
+        out.textContent = 'уже бухой чтоли?';
+        count = 0;
+    }
 });
 
+
+//кнопка очистки всех полей
 clearBtn.addEventListener('click', function() {
-    name1.value = '';
-    name2.value = '';
+    price1.value = '';
+    price2.value = '';
     liters1.value = '';
     liters2.value = '';
     out.textContent = '...';
-    name1.style.boxShadow = '0 0 10px rgb(218, 233, 6)';
+    price1.style.boxShadow = '0 0 10px rgb(218, 233, 6)';
     liters2.style.boxShadow = '0 0 10px rgb(218, 233, 6)';
     out.style.boxShadow = '0 0 10px rgb(218, 233, 6)';
 });
 
+
+//кнопка "сбросить"
 qwestionBtn.addEventListener('click', function() {
     overlay.style.display = 'block';
 });
 
+
+//кнопка "я все понял"
 overlayBtn.addEventListener('click', function() {
     overlay.style.display = 'none';
 });
